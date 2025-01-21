ff423cab460b437657c1e913f38d7dfbf7b874f8
Israel Rotimi <izzyrotimi@gmail.com>
2025-01-21 16:43:21 +0100

    updated Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,30 +23,3 @@ const Home = () => {
 };
 
 export default Home;
-
-export default Home;
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import VideoCard from '../components/VideoCard';
-
-const Home = () => {
-    const [videos, setVideos] = useState([]);
-
-    useEffect(() => {
-        const fetchVideos = async () => {
-            const res = await axios.get('/api/videos');
-            setVideos(res.data);
-        };
-        fetchVideos();
-    }, []);
-
-    return (
-        <div>
-            {videos.map(video => (
-                <VideoCard key={video._id} video={video} />
-            ))}
-        </div>
-    );
-};
-
-export default Home;
